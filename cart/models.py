b4debc0de536878b82742c68e0ed0fd443b8d2c7
Chuc221 <93780475+Chuc221@users.noreply.github.com>
2021-11-07 13:50:55 +0700

    Update models.py

--- cart/models.py
+++ cart/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from shop.models import Item
-from django.contrib.auth.models import User
+from  authorization.models import Customer
 # Create your models here.
 
-# class CartItem(models.Model):
-#     cart = models.ForeignKey(Item, on_delete=models.CASCADE)
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     quantity = models.IntegerField(default=0)
+class Cart(models.Model):
+    user = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.update_at
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.quantity
