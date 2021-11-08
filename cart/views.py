3d6cf04cf9016d8b48872b2b63f5a122853a9a29
Chuc221 <93780475+Chuc221@users.noreply.github.com>
2021-11-08 10:12:40 +0700

    Update views.py

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from shop.models import Item
 
 # Create your views here.
+cart={}
+def addCart(request): 
+    id=request.POST.get('id')
+    num=request.POST.get('quantity')
 
+    proDetail = Item.object.get(pro_id=id)
+
+    if not request.user.is_authenticated: 
+
+        return redirect('/account/login')
+
+    if id in cart.key(): 
+        itemCart={ 
+            'title':proDetail.title,
+            'price':proDetail.price,
+            'category':proDetail.category,
+            'quantity':int(cart[id]['quantity'])+1
+        }
+    else: 
+        itemCart={
+            'title':proDetail.title,
+            'price':proDetail.price,
+            'category':proDetail.category,
+            'quantity':num
+        }
+
+    cart[id]=itemCart
+    request.session['cart']=cart
+    
